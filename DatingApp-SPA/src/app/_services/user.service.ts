5ca09096a4f9e6fb87f34b0e40790aece9d8d815
KuciHoopla <jan.kucera@me.com>
2020-08-18 16:03:51 +0200

    likes

--- DatingApp-SPA/src/app/_services/user.service.ts
+++ DatingApp-SPA/src/app/_services/user.service.ts
@@ -18,7 +18,8 @@ export class UserService {
   getUsers(
     page?,
     itemsPerPage?,
-    userParams?
+    userParams?, 
+    likesParam?
   ): Observable<PaginatedResult<User[]>> {
     const paginatedResult: PaginatedResult<User[]> = new PaginatedResult<
       User[]
@@ -37,6 +38,17 @@ export class UserService {
       params = params.append('gender', userParams.gender);
       params = params.append('orderBy', userParams.orderBy);
     }
+
+    if (likesParam === 'Likers')
+    {
+      params = params.append('likers', 'true');
+    }
+
+    if (likesParam === 'Likees')
+    {
+      params = params.append('likees', 'true')
+    }
+
     return this.http
       .get<User[]>(this.baseUrl + 'users', { observe: 'response', params })
       .pipe(
@@ -68,5 +80,10 @@ export class UserService {
 
   deletePhoto(userId: number, id: number) {
     return this.http.delete(this.baseUrl + 'users/' + userId + '/photos/' + id);
+  }
+
+  sendLike(id: number, recipientId: number)
+  {
+    return this.http.post(this.baseUrl + 'users/' + id + '/like/' + recipientId, {});
   }
 }
--- DatingApp-SPA/src/app/lists/lists.component.ts
+++ DatingApp-SPA/src/app/lists/lists.component.ts
@@ -3,6 +3,8 @@ import { UserService } from '../_services/user.service';
 import { AlertifyService } from '../_services/alertify.service';
 import { User } from '../_models/user';
 import { ActivatedRoute } from '@angular/router';
+import { Pagination, PaginatedResult } from '../_models/pagination';
+import { AuthService } from '../_services/auth.service';
 
 @Component({
   selector: 'app-lists',
@@ -11,17 +13,38 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ListsComponent implements OnInit {
   users: User[];
+  pagination: Pagination;
+  likesParam: string;
+
 
   constructor(
     private userService: UserService,
     private alertify: AlertifyService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute, 
+    private authService: AuthService
   ) { }
 
   ngOnInit() {
     this.route.data.subscribe(data => {
-      // tslint:disable-next-line: no-string-literal
-      this.users = data['users'];
+      this.users = data['users'].result;
+      this.pagination = data['users'].pagination;
     });
+    this.likesParam = 'Likers'
+  }
+
+  loadUsers() {
+    this.userService
+    .getUsers(this.pagination.currentPage, this.pagination.itemsPerPage, null, this.likesParam)
+    .subscribe((res: PaginatedResult<User[]>) => {
+      this.users = res.result;
+      this.pagination = res.pagination;
+    }, error => {
+      this.alertify.error(error);
+    });
+  }
+
+  pageChanged(event: any): void {
+    this.pagination.currentPage = event.page;
+    this.loadUsers();
   }
 }
