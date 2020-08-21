56de0b010384e2fd3c67421b1929e042d6a8cc24
KuciHoopla <jan.kucera@me.com>
2020-08-21 14:27:29 +0200

    messages 11

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -38,6 +38,7 @@ import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
 import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
 import { FileUploadModule } from 'ng2-file-upload';
 import { ListsResolver } from './_resolvers/lists.resolver';
+import { MessagesResolver } from './_resolvers/messages.resolver';
 
 
 export function tokenGetter() {
@@ -92,7 +93,8 @@ export function tokenGetter() {
     MemberListResolver,
     MemberEditResolver,
     PreventUnsavedChanges, 
-    ListsResolver
+    ListsResolver, 
+    MessagesResolver
   ],
 
   bootstrap: [AppComponent],
